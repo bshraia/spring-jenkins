2be2a799a6363f6ab442bd1079d732bf8e57a998
Bexa <bexaray@hotmail.es>
2024-02-06 16:14:00 +0100

    first commit

---
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.javatechie.jenkin.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+class SpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		log.info("Test case executing...");
+		assertEquals(true, true);
+	}
+
+}
