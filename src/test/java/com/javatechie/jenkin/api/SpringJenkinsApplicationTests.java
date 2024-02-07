89d2f761fcda50bd21c3977d34db35400314a2a4
Bexa <bexaray@hotmail.es>
2024-02-07 13:34:54 +0100

    forced build error

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 class SpringJenkinsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	 contextLoads() {
 		log.info("Test case executing...");
 		log.info("Test case executing second log statement...");
 		assertEquals(true, true);
