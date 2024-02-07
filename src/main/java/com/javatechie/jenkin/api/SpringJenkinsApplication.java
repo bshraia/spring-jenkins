9fbd0f9e76904bac0000ed5f06e1f11f14a98be3
Bexa <bexaray@hotmail.es>
2024-02-07 12:07:26 +0100

    segundo commit

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -17,6 +17,7 @@ public class SpringJenkinsApplication {
 	
 	public static void main(String[] args) {
 		log.info("Application executed...");
+		log.info("Application executed second log statement...");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
