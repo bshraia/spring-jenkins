7f651682616534a9ba0d8888c4c0b5a7b61eb6ca
Bexa <bexaray@hotmail.es>
2024-02-09 12:16:30 +0100

    nuevo cambio erroneo

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 class SpringJenkinsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	 contextLoads() {
 		log.info("Test case executing...");
 		assertEquals(true, true);
 	}
